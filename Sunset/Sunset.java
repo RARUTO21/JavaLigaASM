e555b62b1a019ce08e6720ebc0c1a7d9228da20a
Anthony <Anthonylle@hotmail.com>
2016-10-17 22:20:00 -0600

    Cambios
    TODO

--- Sunset/Sunset.java
+++ Sunset/Sunset.java
@@ -1,3 +1,5 @@
+import javax.swing.JFileChooser;
+
 public class Sunset {
 
 	public native float sumar(float a, float b); 
@@ -5,23 +7,25 @@ public class Sunset {
 	public native float multiplicar(float a, float b); 
 	public native float dividir(float a, float b);
 	public native float comparar(float a, float b);
+
+	JFileChooser jfcAbrir = new JFileChooser();
 	
 	static { 
-		System.loadLibrary("operacionesImpl"); 
+		System.loadLibrary("sunsetImpl"); 
 	} 
 	
 	float paso1(float day, float month, float year){
-		float N1 = Math.floor(divir(multiplicar(275.0f,month),9.0f));
-		float N2 = Math.floor(divir(sumar(9.0f,month),12.0f));
-		float N3 = sumar(1.0f,dividir(sumar(restar(year, multiplicar(4.0f, Math.floor(divir(year,4.0f)))), 2.0f), 3.0f));
+		float N1 = (float) Math.floor(dividir(multiplicar(275.0f,month),9.0f));
+		float N2 = (float) Math.floor(dividir(sumar(9.0f,month),12.0f));
+		float N3 = sumar(1.0f,dividir(sumar(restar(year, multiplicar(4.0f, (float)Math.floor(dividir(year,4.0f)))), 2.0f), 3.0f));
 		return restar(sumar(restar(N1, multiplicar(N2, N3)), day), 30.0f);
 	}
 	
-	float paso2Salida(float longitude, float N){
+	float paso2Salida(float lngHour, float N){
 		return sumar(N, dividir(restar(6.0f, lngHour), 24.0f));
 	}
 	
-	float paso2Puesta(float longitude, float N){
+	float paso2Puesta(float lngHour, float N){
 		return sumar(N, dividir(restar(18.0f, lngHour), 24.0f));
 	}
 	
@@ -36,17 +40,17 @@ public class Sunset {
 	}
 	
 	float paso4(float M){
-		return sumar(sumar(sumar(multiplicar(1.916f,Math.sin(M)),multiplicar(0.020f*Math.sin(multiplicar(2.0f,M)))),282.634f),M);
+		return sumar(sumar(sumar(multiplicar(1.916f,(float) Math.sin(M)),multiplicar(0.020f, (float) Math.sin(multiplicar(2.0f,M)))),282.634f),M);
 	}
 	
 	float paso5A(float L){
-		return Math.atan(multiplicar(0.91764f, Math.tan(L)) //Retorna M
+		return (float) Math.atan(multiplicar(0.91764f, (float) Math.tan(L))); //Retorna M
 		//RA potentially needs to be adjusted into the range [0,360) by adding/subtracting 360
 	}
 	
 	float paso5B(float L, float RA){
-		float Lquadrant = multiplicar((Math.floor(dividir(L, 90.0f)), 90.0f);
-		float RAquadrant = multiplicar((Math.floor(dividir(RA, 90.0f)), 90.0f);
+		float Lquadrant = multiplicar( (float) Math.floor(dividir(L,90.0f)),90.0f);
+		float RAquadrant = multiplicar( (float) Math.floor(dividir(RA,90.0f)),90.0f);
 		return sumar(RA, restar(Lquadrant, RAquadrant)); // retorna el RA
 	}
 	
@@ -55,16 +59,16 @@ public class Sunset {
 	}
 	
 	float paso6sinDec(float L){
-		return multiplicar(0.39782f,Math.sin(L));
+		return multiplicar(0.39782f, (float)Math.sin(L));
 	}
 	
 	float paso6cosDec(float sinDec){
-		return Math.cos(Math.asin(sinDec));
+		return (float) Math.cos((float)Math.asin(sinDec));
 	}
 	
 	//calcula el angulo local de la hora del sol
-	float paso7ACosH(float zenith, float latitude, float sinDec){
-		return dividir(restar(Math.cos(zenith), multiplicar(sinDec, Math.sin(latitude))), multiplicar(cosDec, Math.cos(latitude)));
+	float paso7ACosH(float zenith, float latitude, float sinDec, float cosDec){
+		return dividir(restar((float) Math.cos(zenith), multiplicar(sinDec, (float) Math.sin(latitude))), multiplicar(cosDec, (float) Math.cos(latitude)));
 	}
 	
 	void paso7A(float cosH){
@@ -77,11 +81,11 @@ public class Sunset {
 	}
 	
 	float paso7BSalida(float cosH){
-		return restar(360.0f, Math.acos(cosH));
+		return restar(360.0f, (float) Math.acos(cosH));
 	}
 	
 	float paso7BPuesta(float cosH){
-		return Math.acos(cosH);
+		return (float) Math.acos(cosH);
 	}
 	float paso7BFinal(float H){
 		return dividir(H, 15.0f);
@@ -101,11 +105,11 @@ public class Sunset {
 	
 	
 	public static void main(String[] args) { 
-		float day;
-		float month;
-		float year;
-		float latitude;
-		float longitude;
+		float day = 21.0f;
+		float month = 9.0f;
+		float year = 1994.0f;
+		float latitude = 9.52f;
+		float longitude = 83.55f;
 		float zenith = 90.0f;
 		float N;
 		float lngHour;
@@ -120,27 +124,46 @@ public class Sunset {
 		float sinDec;
 		float cosDec;
 		float tUpper;
+		float localOffset = -6.0f;
 		
 		Sunset algoritmo = new Sunset();
+
+		//jfcAbrir.show();
 		
 		N = algoritmo.paso1(day,month,year);
 		lngHour = algoritmo.paso2Fijo(longitude);
 		tLower = algoritmo.paso2Salida(longitude,N);
 		M = algoritmo.paso3(tLower);
+		
 		L = algoritmo.paso4(M);
+		if(L > 360)
+		   L-=360;
+		if(L < 0)
+		   L+=360;
+		
 		RA = algoritmo.paso5A(L);
+		if(RA > 360)
+		   RA-=360;
+		if(RA < 0)
+		   RA+=360;
+		
 		RA = algoritmo.paso5B(L,RA);
 		RA = algoritmo.paso5C(RA);
 		
 		sinDec = algoritmo.paso6sinDec(L);
 		cosDec = algoritmo.paso6cosDec(sinDec);
 		
-		cosH = algoritmo.paso7ACosH(zenith,latitude,sinDec);
-		algotirmo.paso7A(cosH);
+		cosH = algoritmo.paso7ACosH(zenith,latitude,sinDec,cosDec);
+		algoritmo.paso7A(cosH);
 		H = algoritmo.paso7BSalida(cosH);
 		H = algoritmo.paso7BFinal(H);
 		tUpper = algoritmo.paso8(H,RA,tLower);
+		
 		UT = algoritmo.paso9(tUpper,lngHour);
+		if(UT > 24)
+		   UT-=24;
+		if(UT < 0)
+		   UT+=24;
 		
 		localT = algoritmo.paso10(UT,localOffset);
 	} 
